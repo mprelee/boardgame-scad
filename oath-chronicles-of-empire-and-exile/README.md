38b67129cc17856d67c45c675ca453bc8b542199
mprelee <mprelee@gmail.com>
2020-02-03 20:58:41 -0600

    Adds a build script and some pictures

---
+++ oath-chronicles-of-empire-and-exile/README.md
@@ -0,0 +1,5 @@
+# Oath: Chronicles of Empire and Exile
+
+See BGG page:
+
+https://boardgamegeek.com/boardgame/291572/oath-chronicles-empire-and-exile
--- oath-chronicles-of-empire-and-exile/build.sh
+++ oath-chronicles-of-empire-and-exile/build.sh
@@ -1,2 +1,5 @@
 #/usr/bin/env bash
-openscad -o mini_favor.stl mini_favor.scad
+openscad -o mini_favor.stl  mini_favor.scad
+openscad -o mini_magic.stl  mini_magic.scad
+openscad -o poker_favor.stl poker_favor.scad
+openscad -o poker_magic.stl poker_magic.scad
